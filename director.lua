cf5d9e105654f8e9cb4072acc6308fca9d2df3c6
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-11-21 23:07:52 +0000

    finish 1st version

--- director.lua
+++ director.lua
@@ -8,11 +8,11 @@ function director_init()
 end
 
 function director_update()
-  if (stat(54) == 3 and not spawn_ufo) then ufo_init(); spawn_ufo=true end
-  if (stat(54) == 5 and not spawn_firework) then firework_init(); spawn_firework=true end
-  if (stat(54) == 9 and not spawn_tesla) then tesla_init(); spawn_ufo=false; spawn_firework=false; spawn_tesla=true end
-  if (stat(54) == 12 and not spawn_beam) then beam_init(); spawn_beam=true end
-  if (stat(54) == 17 and not spawn_worm) then worm_init(); spawn_beam=false; spawn_worm=true end
-  if (stat(54) == 21 and not spawn_ufo) then ufo_init(); spawn_ufo=true end
-  if (stat(54) == 25) then gamewon_init(); spawn_worm=false; spawn_ufo=false end
+  if (stat(54) == 3 and not spawn_ufo) then spawn_ufo=true; ufo_init(); end
+  if (stat(54) == 5 and not spawn_firework) then spawn_firework=true; firework_init(); end
+  if (stat(54) == 9 and not spawn_tesla) then spawn_ufo=false; spawn_firework=false; spawn_tesla=true; tesla_init(); end
+  if (stat(54) == 12 and not spawn_beam) then spawn_beam=true; beam_init(); end
+  if (stat(54) == 17 and not spawn_worm) then spawn_tesla=false; spawn_beam=false; spawn_worm=true; worm_init(); end
+  if (stat(54) == 21 and not spawn_ufo) then spawn_ufo=true; ufo_init(); end
+  if (stat(54) == 25) then spawn_worm=false; spawn_ufo=false; gamewon_init(); end
 end
